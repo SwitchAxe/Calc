d58ad095ab9f87cabc36904fad03c645ef0f86bc
SwitchAxe <sofiacerasuoli@gmail.com>
2021-07-08 18:47:52 +0200

    removed unnecessary lines

--- lib/calc.c
+++ lib/calc.c
@@ -306,99 +306,6 @@ bool isOperator(char* c)
 	}
 }
 
-queue* no_resort(char** tokens)
-{
-/*	while there are tokens to be read:
-	    read a token.
-	    if the token is a number, then:
-	        push it to the output queue.
-	    else if the token is an operator then:
-	        while ((there is an operator at the top of the operator stack)
-	              and ((the operator at the top of the operator stack has greater precedence)
-	                  or (the operator at the top of the operator stack has equal precedence and the token is left associative))
-	              and (the operator at the top of the operator stack is not a left parenthesis)):
-	            pop operators from the operator stack onto the output queue.
-	        push it onto the operator stack.
-	    else if the token is a left parenthesis (i.e. "("), then:
-	        push it onto the operator stack.
-	    else if the token is a right parenthesis (i.e. ")"), then:
-	        while the operator at the top of the operator stack is not a left parenthesis:
-	            pop the operator from the operator stack onto the output queue.
-	        // If the stack runs out without finding a left parenthesis, then there are mismatched parentheses. //
-	        if there is a left parenthesis at the top of the operator stack, then:
-	            pop the operator from the operator stack and discard it
-	        if there is a function token at the top of the operator stack, then:
-	            pop the function from the operator stack onto the output queue.
-	// After while loop, if operator stack not null, pop everything to output queue //
-	if there are no more tokens to read then:
-	    while there are still operator tokens on the stack:
-	        // If the operator token on the top of the stack is a parenthesis, then there are mismatched parentheses. //
-	        pop the operator from the operator stack onto the output queue.
-	exit. */
-	stack* stk = createStack(256);
-	queue* q = createQueue(256);
-	int i = 0;
-	char* endptr;
-	while (tokens[i] != NULL)
-	{
-		// if (stk->used == 0)
-		// {
-		// 	int i = 0;
-		// 	while (tokens[i] != NULL)
-		// 	{
-		// 		if (isOperator(tokens[i]))
-		// 		{
-		// 			stackPush(stk, tokens[i]);
-		// 			break;
-		// 		}
-		// 		i++;
-		// 	}
-		// }
-
-		strtold(tokens[i], &endptr);
-		if (endptr != tokens[i])
-		{
-			queuePush(q, tokens[i]);
-		}
-		else if (isOperator(tokens[i]))
-		{
-			while (stk->used > 0 &&
-				   (stk->stack[stk->used-1] != NULL)  &&
-				   (calcPrecedence(stk->stack[stk->used-1][0]) >
-				   calcPrecedence(tokens[i][0])) &&
-				   (stk->stack[stk->used-1][0] != '('))
-			{
-				queuePush(q, stackPop(stk));
-			}
-			stackPush(stk, tokens[i]);
-		}
-		else if (!strcmp(tokens[i], "("))
-		{
-			stackPush(stk, tokens[i]);
-		}
-		else if (!strcmp(tokens[i], ")"))
-		{
-			while (strcmp(stk->stack[stk->used-1], "("))
-			{
-				queuePush(q, stackPop(stk));
-			}
-			if (!strcmp(stk->stack[stk->used-1], "("))
-			{
-				stackPop(stk);
-			}
-		}
-		i++;
-	}
-	stackPop(stk);
-	for (int e = stk->used;e --> 0;)
-	{
-		queuePush(q, stackPop(stk));
-	}
-	freeStack(stk);
-	return q;
-}
-
-// resort <<<
 queue* resort(char** tokens)
 {
 	/*
@@ -436,7 +343,7 @@ queue* resort(char** tokens)
 	int i = 0;
 	char* endptr;
 
-	while (tokens[i] != NULL) // while there are tokens to be read <<<
+	while (tokens[i] != NULL)
 	{
 		strtold(tokens[i], &endptr);
 		if (endptr != tokens[i])
@@ -471,9 +378,7 @@ queue* resort(char** tokens)
 			}
 		}
 		i++;
-	} // >>>
-
-	/* stackPop(stk); */
+	} 
 
 	for(int e = stk->used;e --> 0;)
 	{
@@ -482,7 +387,7 @@ queue* resort(char** tokens)
 	freeStack(stk);
 
 	return q;
-} // >>>
+}
 
 long double calc(queue* q)
 {
